698ebcf960464c634d81f9c59f535ecc12bae091
Rlss91 <rlss91@gmail.com>
2022-07-26 20:50:56 +0300

    js function

---
+++ js/login.js
@@ -0,0 +1,22 @@
+console.log("hello world!");
+console.log(5);
+// alert("hello world!");
+1 + 1;
+console.log(1 + 1);
+console.log("1 + 1");
+
+let x = 5; // define variable called x
+let sum = 8; // define variable called sum
+sum = sum + 5; //add to sum 5 => 8 + 5
+console.log(sum); //print the value of sum
+
+//declare function
+function printHelloWorld() {
+  console.log("hello world from function");
+}
+
+printHelloWorld(); //call to function
+
+function handleBtnClick() {
+  console.log("you clicked on a btn");
+}
