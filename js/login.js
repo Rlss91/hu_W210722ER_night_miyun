ed7aaa45d06aa14313e6385a40d6b56715445174
Rlss91 <rlss91@gmail.com>
2022-07-28 20:47:27 +0300

    28072022

--- js/login.js
+++ js/login.js
@@ -1,3 +1,6 @@
+const emailFromServer = "kenny@gmail.com";
+const passwordFromServer = "Aa123456";
+
 console.log("hello world!");
 console.log(5);
 // alert("hello world!");
@@ -7,8 +10,12 @@ console.log("1 + 1");
 
 let x = 5; // define variable called x
 let sum = 8; // define variable called sum
-sum = sum + 5; //add to sum 5 => 8 + 5
+// sum = sum + 5; //add to sum 5 => 8 + 5
+sum += 5;
 console.log(sum); //print the value of sum
+//*TK: add to x 8 and display it in the console
+x += 8;
+console.log(x);
 
 //declare function
 function printHelloWorld() {
@@ -19,4 +26,31 @@ printHelloWorld(); //call to function
 
 function handleBtnClick() {
   console.log("you clicked on a btn");
+  /*
+    document.getElementById will go to html,
+    and will try to find the tag that has id="emailInput".
+    .value will store the data that the user write in to the input tag.
+  */
+  let emailInput = document.getElementById("emailInput").value;
+  console.log("emailInput", emailInput);
+  let passwordInput = document.getElementById("passwordInput").value;
+  console.log("passwordInput", passwordInput);
+  if (emailInput == emailFromServer) {
+    console.log("your email is good!");
+  } else {
+    console.log("your email is bad!");
+  }
+  if (passwordInput == passwordFromServer) {
+    console.log("your password is good!");
+  } else {
+    console.log("your password is bad!");
+  }
+  if (emailInput == emailFromServer && passwordInput == passwordFromServer) {
+    console.log("you allowed to enter");
+    console.log("your email is " + emailInput);
+    console.log("your password is " + passwordInput);
+    window.location.href = "register.html"; //will transfer the user to register.html page
+  } else {
+    console.log("your email or password is incorrect");
+  }
 }
